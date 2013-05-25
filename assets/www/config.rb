c750aa2268ae547e671eea95f7bff5a397514fab
Stanley Lai <stanleylai.sg@gmail.com>
2013-05-25 00:20:50 -0700

    Stripping out JQM. Sidr menu implementation for nag
    Signed-off-by: Stanley Lai <stanleylai.sg@gmail.com>

--- assets/www/config.rb
+++ assets/www/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 fonts_dir = "fonts"
 
 output_style = :compressed
